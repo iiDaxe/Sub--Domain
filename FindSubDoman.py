9cf406154946f33330fb1246c2bf2548ef50178c
iiDaxe <58622512+iiDaxe@users.noreply.github.com>
2020-04-26 17:36:50 +0400

    Add files via upload

---
+++ FindSubDoman.py
@@ -0,0 +1,23 @@
+import base64
+#C0der By Mr.Daxe
+
+
+exec(base64.b64decode("aW1wb3J0IHJlcXVlc3RzCmltcG9ydCByZQpmcm9tIHRlcm1jb2xvciBpbXBvcnQgY3ByaW50Cgpsb2dvID0gIiIiCiQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCcgICAgICAgICAgICAgICBgJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkICAKJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCcgICAgICAgICAgICAgICAgICAgYCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQKJCQkJ2AkJCQkJCQkJCQkJCQkJ2AkJCQkJCQhICAgICAgICAgICAgICAgICAgICAgICAhJCQkJCQkJ2AkJCQkJCQkJCQkJCQkJ2AkJCQKJCQkJCAgJCQkJCQkJCQkJCQgICQkJCQkJCQgICAgICAgICAgICAgICAgICAgICAgICAgJCQkJCQkJCAgJCQkJCQkJCQkJCQgICQkJCQKJCQkJC4gYCQnIFwnIFwkYCAgJCQkJCQkJCEgICAgICBGaW5kIFN1YkRvYW1pbiAgICAgISQkJCQkJCQgICckLyBgLyBgJCcgLiQkJCQKJCQkJCQuICFcICBpICBpIC4kJCQkJCQkJCAgICAgICAgIEJ5IE1yLkRheGUgICAgICAgICQkJCQkJCQkLiBpICBpICAvISAuJCQkJCQKJCQkJCQkICAgYC0tYC0tLiQkJCQkJCQkJCAgICAgICAgICAgICAmICAgICAgICAgICAgICQkJCQkJCQkJC4tLSctLScgICAkJCQkJCQKJCQkJCQkTCAgICAgICAgYCQkJCQkXl4kJCAgICAgICAgICAgQmVyb25pICAgICAgICAgICQkXl4kJCQkJCcgICAgICAgIEokJCQkJCQKJCQkJCQkJC4gICAuJyAgICIifiAgICQkJCAgICAkLiAgICAgICAgICAgICAgICAgLiQgICQkJCAgIH4iIiAgIGAuICAgLiQkJCQkJCQKJCQkJCQkJCQuICA7ICAgICAgLmUkJCQkJCEgICAgJCQuICAgICAgICAgICAgIC4kJCAgISQkJCQkZSwgICAgICA7ICAuJCQkJCQkJCQKJCQkJCQkJCQkICAgYC4kJCQkJCQkJCQkJCQgICAgICQkJC4gICAgICAgICAuJCQkICAgJCQkJCQkJCQkJCQkLicgICAkJCQkJCQkJCQKJCQkJCQkJCQgICAgLiQkJCQkJCQkJCQkJCQhICAgICAkJGAkJCQkJCQkJCckJCAgICAhJCQkJCQkJCQkJCQkJC4gICAgJCQkJCQkJCQKJEpUJnlkJCAgICAgJCQkJCQkJCQkJCQkJCQkJC4gICAgJCAgICAkJCAgICAkICAgLiQkJCQkJCQkJCQkJCQkJCQgICAgICRieSZUTCQKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgJCAgICAkJCAgICAkCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICQuICAgJCQgICAuJAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICBgJCAgICAgICAgJCcKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGAkJCQkJCQkJAoiIiIKY3ByaW50KGxvZ28sICdjeWFuJykKaW5mbyA9ICIgRW50ZXIgWW91ciBUYXJnZXQgXG4gRXggOiAoZ29vZ2xlIG9yIHlhaG9vIC4uLiApICxcbiBEb250IFdyaXRlIGh0dHAvaHR0cHMgYW5kIERvbnQgd3JpdGUgKC5jb20pIG9ubHkgTmFtZSBGb3IgWW91ciBUYXJnZXQgICIKcHJpbnQoIiAiKQpjcHJpbnQoaW5mbywgJ2JsdWUnKQpwcmludCgiICIpCnRhcmdldCA9IGlucHV0KCJFbnRlciBZb3VyIFRhcmdldCAgOiAgIikKY3JmID0gImh0dHBzOi8vY3J0LnNoLz9xPSIKY29tID0gIi5jb20iCnVybCA9IGNyZiArIHRhcmdldCArIGNvbQpyZXMgPSByZXF1ZXN0cy5nZXQodXJsKQpmaW5kdGQgPSByJzxURD4oLiopPC9URD4nCnNyYyA9IHJlcy50ZXh0CmZpbmRsID0gcmUuZmluZGFsbChmaW5kdGQsIHNyYykKZmluZGluZ3MgPSBbXQpmb3IgZm91bmQgaW4gZmluZGw6CiAgICB0ZXN0aW5nID0gZm91bmQubG93ZXIoKQogICAgaWYgIjxicj4iIGluIHRlc3Rpbmc6CiAgICAgICAgZm9yIHNwbGl0ZWQgaW4gdGVzdGluZy5zcGxpdCgiPGJyPiIpOgogICAgICAgICAgICBmaW5kaW5ncy5hcHBlbmQoc3BsaXRlZCkKd2l0aCBvcGVuKCdTdWJkb21haW50LnR4dCcsIG1vZGU9J3cnKSBhcyBmOgogICAgZm9yIGZvdW5kIGluIGZpbmRpbmdzOgogICAgICAgIGYud3JpdGUoZm91bmQgKyAiXG4iKQoKcHJpbnQoIioiKjcwKQpjcHJpbnQoIiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgRG9uZSAgLiAgICIsICd3aGl0ZScpCnByaW50KCIqIio3MCkK"))
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'               `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$'                   `$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# $$$'`$$$$$$$$$$$$$'`$$$$$$!                       !$$$$$$'`$$$$$$$$$$$$$'`$$$
+# $$$$  $$$$$$$$$$$  $$$$$$$                         $$$$$$$  $$$$$$$$$$$  $$$$
+# $$$$. `$' ' \$`  $$$$$$$!      Find SubDoamin     !$$$$$$$  '$/ `/ `$' .$$$$
+# $$$$$. !\  i  i .$$$$$$$$         By Mr.Daxe        $$$$$$$$. i  i  /! .$$$$$
+# $$$$$$   `--`--.$$$$$$$$$             &             $$$$$$$$$.--'--'   $$$$$$
+# $$$$$$L        `$$$$$^^$$           Beroni          $$^^$$$$$'        J$$$$$$
+# $$$$$$$.   .'   ""~   $$$    $.                 .$  $$$   ~""   `.   .$$$$$$$
+# $$$$$$$$.  ;      .e$$$$$!    $$.             .$$  !$$$$$e,      ;  .$$$$$$$$
+# $$$$$$$$$   `.$$$$$$$$$$$$     $$$.         .$$$   $$$$$$$$$$$$.'   $$$$$$$$$
+# $$$$$$$$    .$$$$$$$$$$$$$!     $$`$$$$$$$$'$$    !$$$$$$$$$$$$$.    $$$$$$$$
+# $JT&yd$     $$$$$$$$$$$$$$$$.    $    $$    $   .$$$$$$$$$$$$$$$$     $by&TL$
+#                                  $    $$    $
+#                                  $.   $$   .$
+#                                  `$        $'
+#                                   `$$$$$$$$
